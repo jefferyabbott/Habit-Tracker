99fe5ac8f8dfcdfb97614132af16627857af414b
Jeffery Abbott <jeff@jefferyabbott.com>
2024-10-05 08:14:43 -0400

    Create and display habits

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/components/side_drawer.dart';
 import 'package:habit_tracker/database/habit_database.dart';
+import 'package:habit_tracker/utils/habit_utilities.dart';
 import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
+import '../models/habit.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    Provider.of<HabitDatabase>(context, listen: false).readHabits();
+    super.initState();
+  }
 
   final TextEditingController textController = TextEditingController();
 
@@ -56,6 +70,22 @@ class HomePage extends StatelessWidget {
           color: Colors.black,
         ),
       ),
+      body: _buildHabitList(),
+    );
+  }
+
+  Widget _buildHabitList() {
+    final habitDatabase = context.watch<HabitDatabase>();
+    List<Habit> currentHabbits = habitDatabase.currentHabits;
+    return ListView.builder(
+      itemCount: currentHabbits.length,
+      itemBuilder: (context, index) {
+        final habit = currentHabbits[index];
+        bool isCompletedToday = isHabitCompletedToday(habit.completedDays);
+        return ListTile(
+          title: Text(habit.name),
+        );
+      },
     );
   }
 }
